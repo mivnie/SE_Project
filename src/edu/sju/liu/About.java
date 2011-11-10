4f378c16723bf26c428ecd34fdaf0ee170a5ce1e
Xiaohui Liu <hahahalxh@hotmail.com>
2011-11-10 16:57:43 -0500

    test

--- src/edu/sju/liu/About.java
+++ src/edu/sju/liu/About.java
@@ -7,6 +7,6 @@ public class About extends Activity {
 	protected void onCreate(Bundle savedInstanceState){
 	super.onCreate(savedInstanceState)	;
 	setContentView(R.layout.about);
-        //test	
+        //
 }
 }
