c8294d7bccf3ae321927afe660fce96a7c1dd54e
Xiaohui Liu <hahahalxh@hotmail.com>
2011-11-12 18:01:19 -0500

    public class About extends Activity {  	protected void onCreate(Bundle savedInstanceState){  	super.onCreate(savedInstanceState)	;  	setContentView(R.layout.about);         } //<<<<<< HEAD
    //}
    //>>>>>>> Draw a base image on the canvas
    
    
    //Draw a base image on the canvas
     }
    //No newline at end of file

--- src/edu/sju/tankwar/About.java
+++ src/edu/sju/tankwar/About.java
@@ -13,10 +13,7 @@ public class About extends Activity {
 	protected void onCreate(Bundle savedInstanceState){
 	super.onCreate(savedInstanceState)	;
 	setContentView(R.layout.about);
-<<<<<<< HEAD
-        
-}
-=======
-	}
->>>>>>> Draw a base image on the canvas
+    }
+	
+  //Draw a base image on the canvas
 }
